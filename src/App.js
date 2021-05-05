f7cb4e1771171831b3f9ad6f2ee5dcd3977e6e1c
Andrea Gallo <andreagallo264@gmail.com>
2021-05-05 16:48:45 -0300

    implement-materialUI - changed favicon - installed materialUI - implemented materialUI components in App to test it was OK

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import AccessAlarmIcon from "@material-ui/icons/AccessAlarm";
+import { Button, Typography } from "@material-ui/core";
+
 function App() {
   return (
     <div className="App">
-      <h1>Todo app</h1>
+      <Typography variant="h5" component="h5">
+        <AccessAlarmIcon color="primary" />
+        Todo App
+      </Typography>
+      <Button variant="contained" color="secondary">
+        Primary
+      </Button>
     </div>
   );
 }
