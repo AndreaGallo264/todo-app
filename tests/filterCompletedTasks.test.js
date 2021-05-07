2034f812982844e23bf2dc3b061f049e32583845
Andrea Gallo <andreagallo264@gmail.com>
2021-05-07 15:57:31 -0300

    changes: - changed readme file with app instructions - changed a variable name to fit better its content

--- tests/filterCompletedTasks.test.js
+++ tests/filterCompletedTasks.test.js
@@ -5,8 +5,16 @@ fixture `Tasks suite`.page `http://localhost:3000/`;
 
 //Selectors
 const filterBtn = Selector("header > div > button");
-const completedItem = Selector("ul > div.Mui-selected");
+const completedTask = Selector("ul > div.Mui-selected");
+const noTasksText = Selector("ul > div > p");
 
 test("Filter completed tasks", async (t) => {
-  await t.click(filterBtn).expect(completedItem.exists).notOk();
+  await t.click(filterBtn).expect(completedTask.exists).notOk();
+  const pendingTasks = Selector("ul > div");
+  var count = await pendingTasks.count;
+  for (var i = 0; i < count; i++) await t.click(pendingTasks.nth(i));
+  await t
+    .click(filterBtn)
+    .expect(noTasksText.innerText)
+    .contains("You have no tasks");
 });
